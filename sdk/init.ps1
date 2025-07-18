beb327dd97d19f0dc30b457e6a1e28c1bff15760
Project Laguardia <project@laguardia.app>
2025-07-17 18:14:03 -0700

    devtools(vcs): :sparkles: automatically check if default url is valid

--- sdk/init.ps1
+++ sdk/init.ps1
@@ -1,6 +1,12 @@
 param(
     [string] $Origin,
-    [string] $SDK = "https://github.com/project-laguardia/sdk", # Allows forked contributions
+    [string] $SDK = (& {
+        $default = "https://github.com/project-laguardia/sdk"
+        $common = (git ls-remote $default sdkland) -split "`t" | Select-Object -First 1
+        If( $common -eq "$(git rev-list --max-parents=0 HEAD)" ) {
+            return $default
+        }
+    }), # Allows forked contributions
     [switch] $Branch,
     [switch] $Push
 )
